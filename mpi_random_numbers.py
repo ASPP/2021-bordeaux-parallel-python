acd6897e8f99d3a18ad1b690c549e85a7b781f41
Jakob Jordan <jakobjordan@posteo.de>
2021-08-26 20:56:34 +0200

    Work on random numbers example

--- mpi_random_numbers.py
+++ mpi_random_numbers.py
@@ -1,9 +1,22 @@
 from mpi4py import MPI
 import numpy as np
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
 
-x = np.random.normal(loc=rank, size=10_000)
+comm = MPI.COMM_WORLD  # get MPI communicator
+process = comm.Get_rank()  # get process index
+n_processes = comm.Get_size()  # get total number of processes
 
-print(f"Hello world from rank {rank}. My mean is {np.mean(x)}.")
+# initialize a random number generator for each process
+rng = np.random.RandomState(1235 + process)
+
+# define the number of particles per processes and time steps to simulate
+n_particles_per_process = 3
+n_particles = n_particles_per_process * n_processes
+n_time_steps = 5
+
+# determine work distribution between ranks
+x_min = process / n_processes
+x_max = (process + 1) / n_processes
+x = np.random.uniform(x_min, x_max, size=n_particles_per_process)
+
+print(f"{process=}: particle positions {x}")
